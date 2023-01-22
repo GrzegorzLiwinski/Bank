7242db6ca83e04a694be52ef65e61ad7a575af58
eweleo <e.leonarczyk@gmail.com>
2023-01-22 17:59:04 +0100

    first commit

---
+++ Bank/Bank/Bank.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{ 
+    class Bank
+    {
+        public void menuScreen()
+        {
+            Console.WriteLine("WYBIERZ OPCJĘ:");
+            Console.WriteLine("1 => LISTA WSZYSTKICH KLIENTÓW BANKU");
+            Console.WriteLine("2 => LOGOWANIE");
+            Console.WriteLine("3 => ZAKOŃCZ PROGRAM");
+            Console.WriteLine("WYBIERZ 1, 2 LUB 3:");
+        }
+
+        public void screeen1(List<Client> clients)
+        {
+            Console.WriteLine("ID | IMIĘ I NAZWISKO | NR KONTA | SALDO");
+            clients.ForEach(c => Console.WriteLine(c.id + " | " + c.firstName + " | " + c.lastName + " | " + c.accountNumber + " | " + c.amount));
+            Console.WriteLine("WYBIERZ OPCJĘ:");
+            Console.WriteLine("1 => LISTA WSZYSTKICH KLIENTÓW BANKU");
+            Console.WriteLine("2 => LOGOWANIE");
+            Console.WriteLine("3 => ZAKOŃCZ PROGRAM");
+            Console.WriteLine("WYBIERZ 1, 2 LUB 3:");
+
+         
+        }
+
+
+
+    }
+}
---
+++ Bank/Bank/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Client
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string accountNumber { get; set; }
+        public decimal amount { get; set; }
+
+        Client(int id, string firstName, string lastName, string accountNumber, decimal amount)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.accountNumber = accountNumber;
+            this.amount = amount;
+        }
+
+        public List<Client> createUsers()
+        {
+            List<Client> clients = new List<Client>();
+            clients.Add(new Client(1, "Jan", "Nowak", "001", 1457.23m));
+            clients.Add(new Client(2, "Agnieszka", "Kowalska", "002", 3600.18m));
+            clients.Add(new Client(3, "Robert", "Lewandowski", "003", 2745.03m));
+            clients.Add(new Client(4, "Zofia", "Plucińska", "004", 7344.00m));
+            clients.Add(new Client(5, "Grzegorz", "Braun", "005", 455.38m));
+            return clients;
+        }
+    }
+}
